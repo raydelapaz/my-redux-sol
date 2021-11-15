1fd1bbf375290574150dc0c7988fc4888c5cb961
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-15 15:41:19 -0500

    codegrade base.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { createStore } from 'redux';
 import reducer from './reducers';
 
 import './index.css';
-import App from './App'
+import App from './components/App'
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const store = createStore(reducer);
